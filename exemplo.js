198af2a69cba062ab733ec71a8f1a32652e05e81
abner <castroabner93@gmail.com>
2026-01-06 20:11:26 +0000

    exemplo de requisição de cliente

---
+++ exemplo.js
@@ -0,0 +1,108 @@
+{
+"personType": "F",
+"foreigner": "N",
+"typeId": 1,
+"internationalId": "BR123456789",
+"subtypeIds": [1],
+"addresses": [
+{
+"type": "RESIDENCIAL",
+"streetName": "Rua das Flores",
+"number": "123",
+"complement": "Apto 45",
+"neighborhood": "Centro",
+"cityId": 3550308,
+"city": "São Paulo",
+"state": "SP",
+"zipCode": "01010-000"
+}
+],
+"phones": [
+{
+"number": "11988887777",
+"main": true,
+"type": "CELULAR",
+"note": "Telefone principal",
+"idd": "55"
+}
+],
+"naturalPersonData": {
+"name": "João da Silva",
+"email": "joao.silva@email.com",
+"birthDate": "1985-06-15",
+"birthPlace": "São Paulo - SP",
+"civilStatus": "CASADO",
+"cpf": "12345678901",
+"mailingAddress": "R",
+"licenseNumber": "123456789",
+"licenseIssuingBody": "SSP",
+"licenseIssueDate": "2005-08-10",
+"fatherName": "José da Silva",
+"sex": "M",
+"issueDateIdentityCard": "2003-05-20",
+"matrimonialRegime": "COMUNHÃO_PARCIAL",
+"marriageDate": "2010-09-25",
+"issuingBody": "SSP-SP",
+"nationality": "BRASILEIRA",
+"numberIdentityCard": "12345678",
+"motherName": "Maria da Silva",
+"profession": "Engenheiro Civil",
+"spouse": {
+"foreigner": "N",
+"internationalId": "BR987654321",
+"cpf": "10987654321",
+"name": "Ana da Silva",
+"email": "ana.silva@email.com",
+"sex": "F",
+"civilStatus": "CASADO",
+"birthDate": "1987-03-22",
+"numberIdentityCard": "87654321",
+"issueDateIdentityCard": "2004-07-18",
+"profession": "Arquiteta",
+"nationality": "BRASILEIRA",
+"birthPlace": "Campinas - SP",
+"fatherName": "Carlos Pereira",
+"motherName": "Fernanda Pereira",
+"cellphoneNumber": "11977776666",
+"businessPhone": "1133334444",
+"company": "Escritório de Arquitetura XPTO",
+"addresses": {
+"city": "São Paulo",
+"complement": "Casa",
+"neighborhood": "Vila Mariana",
+"number": "456",
+"streetName": "Rua das Acácias",
+"zipCode": "04010-100"
+}
+}
+},
+"legalPersonData": {
+"name": "Construtora Exemplo LTDA",
+"email": "contato@construtoraexemplo.com.br",
+"cityRegistrationNumber": "12345",
+"cnaeNumber": "4120400",
+"cnpj": "12345678000199",
+"contactName": "Pedro Almeida",
+"creaNumber": "CREA123456",
+"establishmentDate": "2015-04-10",
+"fantasyName": "Construtora Exemplo",
+"note": "Empresa fictícia para testes",
+"site": "https://www.construtoraexemplo.com.br",
+"shareCapital": 500000,
+"stateRegistrationNumber": "987654321",
+"technicalManager": "Eng. Pedro Almeida",
+"agents": [
+{
+"id": 1
+}
+]
+},
+"familyIncome": [
+{
+"kinsName": "Ana da Silva",
+"kinship": "CÔNJUGE",
+"incomeValue": 4500,
+"observation": "Renda mensal como arquiteta"
+}
+]
+}
